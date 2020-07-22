e6a9abd0b61393f62935470db0a8d1f6380097a8
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-07-22 12:35:07 -0500

    jenkins installation for ubuntu added

--- class4/packer/install-jenkins-script.sh
+++ class4/packer/install-jenkins-script.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-sudo yum install epel-release -y
-sudo yum install wget java -y 
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
-sudo rpm --import http://pkg.jenkins.io/redhat/jenkins.io.key
-sudo yum install jenkins -y 
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
+sudo apt -y update
+sudo apt -y install default-jdk
+wget -y -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo -y sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt -y update
+sudo apt -y install jenkins
+sudo systemctl enable --now jenkins -y
+
+
+
+# sudo yum install epel-release -y
+# sudo yum install wget java -y 
+# sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
+# sudo rpm --import http://pkg.jenkins.io/redhat/jenkins.io.key
+# sudo yum install jenkins -y 
+# sudo systemctl start jenkins
+# sudo systemctl enable jenkins
